9dcb49a39fcfbdc4085556c36ff45ddb70d8426d
Aman Diwakar <157607718+amandiwakar86@users.noreply.github.com>
2025-04-01 14:37:46 +0530

    delete login.php waste file

--- login.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['user_id'])) {
-    header("Location: ./jwelry-website/userAuthentication/login.php"); Redirect to login if not logged in
-    exit();
-}
-?>
